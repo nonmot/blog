9ee0cd1c7b106410afc523111ffe730d9f7113e6
Mot <nonmot0614@gmail.com>
2021-05-02 16:18:15 +0900

    add settings for heroku

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,7 +1,11 @@
 from .common import *
+import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASE['default'].update(db_from_env)
