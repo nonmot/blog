d98ebd2b28d8e7e815b4dd6834de1d0035b66480
野中元哉 <nonakamotoya@nonakamotoyanoMacBook-Air.local>
2021-08-23 19:23:13 +0900

    add allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,4 @@
+from config.settings.local import CORS_ORIGIN_ALLOW_ALL, CORS_ORIGIN_WHITELIST
 from .common import *
 import dj_database_url
 
@@ -5,7 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.https://jovial-gates-acd3c5.netlify.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'netlify.app']
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
@@ -19,3 +20,7 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('API_KEY'),
     'API_SECRET': os.environ.get('API_SECRET')
 }
+
+CORS_ORIGIN_WHITELIST = [
+    'https://jovial-gates-acd3c5.netlify.app'
+]
