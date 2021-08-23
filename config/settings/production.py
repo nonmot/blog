7fb982e9c49fc5afedb2ffc9e6ffcfb979703bfb
野中元哉 <nonakamotoya@nonakamotoyanoMacBook-Air.local>
2021-08-23 19:14:40 +0900

    add allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.netlify.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.https://jovial-gates-acd3c5.netlify.app']
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
