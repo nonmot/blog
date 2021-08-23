ed87077f85fa1fbdf22281b0b7693ce62930aa52
野中元哉 <nonakamotoya@nonakamotoyanoMacBook-Air.local>
2021-08-23 19:07:36 +0900

    settings for netlify

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.netlify.app']
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
