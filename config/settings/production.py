b2ac51a7fab9c38b98faa5e0e9d8f098658cdef2
野中元哉 <nonakamotoya@nonakamotoyanoMacBook-Air.local>
2021-08-24 19:48:06 +0900

    fix allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'lovephys.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'lovephys.com', env('ALLOWED_HOST')]
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
