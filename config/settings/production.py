5e5bcb15d7031cb0cc6ca381f48211b40e30fa23
nonmot <nonmot0614@gmail.com>
2021-09-19 15:02:44 +0900

    ロギングの設定

--- config/settings/production.py
+++ config/settings/production.py
@@ -29,3 +29,47 @@ STATIC_ROOT = '/usr/share/nginx/html/static'
 MEDIA_ROOT = '/usr/share/nginx/html/media'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# ロギング
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        },
+        # blogアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['file'],
+        }
+    },
+
+    # ハンドラーの設定
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+            'formatter': 'prod',
+            'when': 'D', # ログローテーション（新しいファイルへの切り替え）間隔の単位
+            'interval': 1, # ログローテーション間隔（1日）
+            'backupCount': 7, # 保存しておくログファイル数
+        }
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'prod': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        }
+    }
+}
